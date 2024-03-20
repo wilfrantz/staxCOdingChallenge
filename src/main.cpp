09e28970b034b14fe0dbd4bd9cc4fa363c98f4dc
wilfrantz <wilfrantz@outlook.com>
2024-03-20 15:39:30 -0400

    Adds more ddata

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,16 @@ int main()
 
     Stax stax;
     std::map<std::string, std::map<std::string, std::string>> domainData{
-        {"domain1", {{"age", "19"}, {"name", "dede.dev"}, {"registrar", "Godaddy LLC"}}},
-        {"domain2", {{"age", "19"}, {"name", "dede.dev"}, {"registrar", "Godaddy LLC"}}}};
+        {"Entry 1", {{"age", "19"}, {"name", "dede.dev"}, {"registrar", "GoDaddy LLC"}}},
+        {"Entry 2", {{"age", "8"}, {"name", "facebook.com"}, {"registrar", "nameCheap LLC"}}},
+        {"Entry 3", {{"age", "3"}, {"name", "staxpayments.com"}, {"registrar", "GoDaddy LLC"}}},
+        {"Entry 4", {{"age", "13"}, {"name", "payment.com"}, {"registrar", "101domain GRS Limited"}}},
+        {"Entry 5", {{"age", "60"}, {"name", "pay.com"}, {"registrar", "SafeNames Ltd."}}}};
 
     const Domain data = stax.loadData(domainData);
     Node node = stax.createNode(data);
     stax.insertAtBeginning(&node);
     stax.printList();
 
-        return (EXIT_SUCCESS);
-}
+    return (EXIT_SUCCESS);
+    }
